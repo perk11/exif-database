bc6cae85b9285ce06d2d09cd0d437459c62eed0f
Konstantin Pereiaslov <perk11@perk11.info>
2023-09-07 23:05:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# exif-database
+
+A bash script to build a sqlite database of all EXIF information in directory.
+
+## Pre-requisites:
+
+exiftool, jq, parallel, sqlite3
+
+**Usage**:
+	
+	./build_exif_dbv2.sh <directory_with_photos> <output_db_file>
+
+The script will try to use all CPU cores and will display a progressbar with the name of the file currently being processed.
+It will try to read all the files, everything recognized by exiftool is going to get indexed.
+
+If ran again with the same arguments, it will skip the files already processed, allowing to resume it if it got interrupted and also only index new files.
+
+DO NOT TRY TO READ OR WRITE FROM THE DATABASE WHILE THE SCRIPT IS RUNNING. This might cause sqlite to error out and some records will fail to INSERT.
+
+# Why
+
+This allows to fairly quickly search photos by exif information. For example, find all photos and videos made on Apple devices:
+
+    sqlite3 database.db "SELECT filename FROM exif_data WHERE json_extract(exif_json, '$.Make') = 'Apple';"
+
+It also allows to pull interesting statistics on photos, for example here how you can pull number of files by device model taken each year, using EXIF and falling back to modification date if EXIF is not available:
+
+	sqlite3 database.db "SELECT substr(coalesce(json_extract(exif_json, '$.SubSecDateTimeOriginal'),\
+	       json_extract(exif_json, '$.FileModifyDate')), 1, 4) AS Year,\
+	       json_extract(exif_json, '$.Make') ||' ' || json_extract(exif_json, '$.Model') AS Model,\
+	       count(1)\
+	 FROM exif_data\
+	 GROUP BY Year, Model\
+	 ORDER BY Year, Model;"
---
+++ build_exif_dbv2.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+DIRECTORY="$1"
+DATABASE_FILE="$2"
+
+if [[ -z "$DIRECTORY" || -z "$DATABASE_FILE" ]]; then
+    echo "Usage: $0 <directory> <output_db_file>"
+    exit 1
+fi
+
+# Ensure exiftool and parallel are installed
+if ! command -v exiftool &> /dev/null; then
+    echo "Error: exiftool not found. Please install it."
+    exit 1
+fi
+
+if ! command -v parallel &> /dev/null; then
+    echo "Error: parallel not found. Please install it."
+    exit 1
+fi
+if ! command -v jq &> /dev/null; then
+    echo "Error: jq not found. Please install it."
+    exit 1
+fi
+if ! command -v sqlite3 &> /dev/null; then
+    echo "Error: sqlite not found. Please install it."
+    exit 1
+fi
+
+# Initialize SQLite database
+sqlite3 "$DATABASE_FILE" "CREATE TABLE IF NOT EXISTS exif_data (filename TEXT, exif_json TEXT);"
+
+# Get filenames that are already in the database
+existing_files=$(sqlite3 "$DATABASE_FILE" "SELECT filename FROM exif_data;")
+
+# Convert list of filenames to a temporary file for efficient filtering
+tempfile=$(mktemp)
+echo "$existing_files" > "$tempfile"
+
+# Define a function for parallel execution
+process_file() {
+    file="$1"
+    JSON_DATA=$(exiftool -json "$file" | jq -c .[0])
+    # Escape single quotes for SQLite insertion
+    ESCAPED_JSON_DATA=$(echo "$JSON_DATA" | sed "s/'/''/g")
+    # Return the result as an INSERT statement
+    echo "INSERT INTO exif_data (filename, exif_json) VALUES ('$file', '$ESCAPED_JSON_DATA');"
+}
+
+export -f process_file
+
+# If existing_files is empty, process all files. Otherwise, exclude existing ones.
+if [ -z "$existing_files" ]; then
+    sqlite3 "$DATABASE_FILE" < <(find "$DIRECTORY" -type f | parallel --bar process_file {})
+else
+    sqlite3 "$DATABASE_FILE" < <(find "$DIRECTORY" -type f | grep -vFf "$tempfile" | parallel --bar process_file {})
+fi
+
+# Clean up
+rm "$tempfile"
